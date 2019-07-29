10cd48e53695b377aebca98f65416969724b4ce7
Kaleb White <kawhite@ancestry.com>
2019-07-29 13:27:18 -0600

    a

--- utils.py
+++ utils.py
@@ -102,8 +102,8 @@ def get_data2(file, filters='!"%;[\\]^_`{|}~\t\n', training_len=50,
     with open(file) as f:
         lineList = f.readlines()
     abstracts = lineList
-    word_idx, idx_word, num_words, word_counts, texts, sequences, features, labels = make_sequences(
-        abstracts, training_len, lower, filters)
+    word_idx, idx_word, num_words, word_counts, texts, sequences, features, labels = make_sequences2(
+        abstracts, lower, filters)
     X_train, X_valid, y_train, y_valid = create_train_valid(features, labels, num_words)
     training_dict = {'X_train': X_train, 'X_valid': X_valid, 
                      'y_train': y_train, 'y_valid': y_valid}
@@ -161,6 +161,59 @@ def create_train_valid(features,
     gc.collect()
 
     return X_train, X_valid, y_train, y_valid
+
+def make_sequences2(texts, lower = True, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n'):
+    """Turn a set of texts into sequences of integers"""
+    
+    # Create the tokenizer object and train on texts
+    tokenizer = Tokenizer(lower=lower, filters=filters)
+    tokenizer.fit_on_texts(texts)
+    
+    # Create look-up dictionaries and reverse look-ups
+    word_idx = tokenizer.word_index
+    idx_word = tokenizer.index_word
+    num_words = len(word_idx) + 1
+    word_counts = tokenizer.word_counts
+    
+    print(f'There are {num_words} unique words.')
+    
+    # Convert text to sequences of integers
+    sequences = tokenizer.texts_to_sequences(texts)
+    
+    # Limit to sequences with more than training length tokens
+    seq_lengths = [len(x) for x in sequences]
+    over_idx = [i for i, l in enumerate(seq_lengths) if l > (training_length + 20)]
+    
+    #new_texts = []
+    #new_sequences = []
+    
+    # Only keep sequences with more than training length tokens
+    #for i in over_idx:
+    #    new_texts.append(texts[i])
+    #    new_sequences.append(sequences[i])
+        
+    features = []
+    labels = []
+    
+    # Iterate through the sequences of tokens
+    for seq in sequences:
+        
+        # Create multiple training examples from each sequence
+        #for i in range(training_length, len(seq)):
+        
+        # Extract the features and label
+        feature = seq[2:]
+        label = seq[1:2]
+        prev = seq[:1]
+        
+        # Set the features and label
+        features.append(feature)
+        labels.append(label)
+    
+    print(f'There are {len(features)} sequences.')
+    
+    # Return everything needed for setting up the model
+    return word_idx, idx_word, num_words, word_counts, texts, sequences, features, labels
 
 def make_sequences(texts, training_length = 50,
                    lower = True, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n'):
@@ -356,7 +409,7 @@ def seed_sequence(model, s, word_idx, idx_word,
             s.append(idx_word[next_idx])
             gen.append(idx_word[next_idx])
         else:
-            for i in range(20):
+            for i in range(40):
                 next_idx = np.argmax(np.random.multinomial(1, preds, size = 1))
                 gen.append(idx_word[next_idx])
         
